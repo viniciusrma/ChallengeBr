a050081960e6eb7fce06ae0d16376c5490e4ac7b
Vinícius Alves <viniciusalves@7itec.online>
2021-04-18 11:28:41 -0300

    chore: api done, created front end project

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChallengeBr</h1>
+    </div>
+  );
+}
+
+export default App;
